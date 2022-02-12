eb177b7541baa74c620183a15a98a81ba9c941c6
Jesus Alexis Moreno Valverde <jesus.morenov@coppel.com>
2022-02-11 18:27:10 -0700

    se agrega healthcheck

---
+++ src/main/java/com/example/sara/controllers/HealtCheckController.java
@@ -0,0 +1,17 @@
+package com.example.sara.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("api/healtcheck")
+public class HealtCheckController {
+
+    @GetMapping
+    public String getHealtCheck(){
+        return "OK";
+    }
+}
